e09ca7f0661d79285e6a723b3bfdc1e6225bfd45
Red alyn of the rosewood Rhaenys targaryen <rrhaenys@vo-e3.21-school.ru>
2019-04-19 19:58:40 +0300

    sphere correct

--- sources/sphere_texture.c
+++ sources/sphere_texture.c
@@ -6,7 +6,7 @@
 /*   By: rrhaenys <rrhaenys@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/18 15:25:00 by rrhaenys          #+#    #+#             */
-/*   Updated: 2019/04/19 19:34:27 by rrhaenys         ###   ########.fr       */
+/*   Updated: 2019/04/19 19:57:50 by rrhaenys         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,13 @@ t_vec3			sphere_texture(t_texture *texture, t_shape *shape)
 	vec3_subtract(&shape->surface_point, &shape->center, &normal);
 	vector_matrix_multiply(shape->rotation, &normal);
 	vec3_normalize(&normal, vec3_length(&normal));
-	u = (0.5 + atan2(normal.z, normal.x) / (2.0 * M_PI));
+	u = -(0.5 + atan2(normal.z, normal.x) / (2.0 * M_PI));
 	v = (0.5 - asin(normal.y) / M_PI);
 	move_texture(&u, &v, (double[2]){shape->t_dims.x, shape->t_dims.y});
 	u = ((u * 2.0) - 1.0);
 
 	if (shape->t_dims.z != 0)
-		return (copy_texture(texture, shape, (double[2]){u, v}));
+		return (copy_texture(texture, shape, (double[2]){u, v / 2.0}));
 	else
 		return (texture_stretching(texture, shape, (double[2]){u, v}));
 }
