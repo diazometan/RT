fe66f521c681eebfa833a67addfd23446208b504
Jefferey Franklyn frey <jfrankly@oa-b2.21-school.ru>
2019-04-20 14:26:24 +0300

    lights added

--- sources/ui_sources/ui_buttons.c
+++ sources/ui_sources/ui_buttons.c
@@ -29,7 +29,7 @@ static void	button_event_exit(t_rtui *ui, t_rt *rt, t_sdl *sdl)
 			free_args(rt->head_shapes, rt->head_light, rt->head_textures);
 		SDL_DestroyWindow(sdl->win);
 		SDL_Quit();
-		exit(1);
+		exit(1); 
 	}
 }
 
